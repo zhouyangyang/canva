dc23233b721c5f407c40f5b2c00ab5bc3d92ae64
zhouyangyang <850754840@qq.com>
2016-01-30 14:13:31 +0800

    init
    dfs

---
+++ js/canvas.js
@@ -0,0 +1,90 @@
+$(function(){
+    var box = $('.box');
+    var copy = $(".copy");
+    var canvas = $("canvas");
+    var cobj =canvas[0].getContext("2d");
+    canvas.attr({
+        width:copy.width(),
+        height:copy.height()
+    })
+    $(".hasson").hover(function(){
+        var index=$(this).index(".h");
+        $(this).find(".son").finish();
+        $(this).find(".son").show(200);
+    },function(){
+
+        $(this).find(".son").finish();
+        var that=this;
+        $("this").find(".son").hover(function(){
+            $(that).show();
+        },function(){$(that).hide();})
+
+        $(this).find(".son").hide(200);
+
+    });
+
+
+
+
+    var obj=new shape(copy[0],canvas[0],cobj);
+    $(".hasson:eq(1)").find(".son li").click(function(){
+
+        if($(this).attr("data-role")!="pen"){
+            obj.shapes=$(this).attr("data-role");
+            obj.draw();
+
+
+        }else{
+            obj.shapes = $(this).attr("data-role");
+            obj.pen();
+        }
+
+    });
+    $(".type li").click(function(){
+        obj.type=$(this).attr("data-role");
+        obj.draw();
+    })
+    $(".linecolor input").change(function(){
+        obj.borderColor=$(this).val();
+    })
+    $(".fillcolor input").change(function(){
+        obj.bgcolor=$(this).val();
+    })
+
+    //橡皮
+    $(".xianpi li").click(function(){
+        var w=$(this).attr("data-role");
+        var h=$(this).attr("data-role");
+        obj.xp($(".xp"),w,h)
+    })
+
+
+    $(".file li").click(function(){
+        var index=$(this).index(".file li");
+        if(index==0){
+            if(obj.history.length>0){
+                var yes=window.confirm("是否要保存");
+                if(yes){
+                    location.href=(canvas[0].toDataURL().replace("data:img/png","stream/octet"))
+                }
+            }
+            obj.history=[];
+            cobj.clearRect(0,0,canvas[0].width,canvas[0].height);
+        }else if(index==1){
+            location.href=(canvas[0].toDataURL().replace("data:img"))
+        }else if(index==2){
+            cobj.clearRect(0,0,canvas[0].width,canvas[0].height);
+            if(obj.history.length==0){
+                alert("不能后退");
+                return
+            }
+            var data=obj.history.pop();
+            cobj.putImageData(data,0,0);
+        }
+    })
+
+    $(".select").click(function(){
+        obj.ss($(".textArea"));
+    })
+
+})
---
+++ js/shape.js
@@ -0,0 +1,240 @@
+function shape(canvas,canvas1,cobj){
+    this.canvas=canvas;
+    this.canvas1=canvas1;
+    this.cobj=cobj;
+    this.bgcolor="#000";
+    this.borderColor="#000";
+    this.lineWidth=1;
+    this.type="stroke";
+    this.shapes="line";
+    this.history=[];
+    this.width=this.canvas1.width;
+    this.height=this.canvas1.height;
+    console.log(this.width);
+}
+shape.prototype={
+    init:function(){
+        this.cobj.fillStyle=this.bgcolor;
+        this.cobj.strokeStyle=this.borderColor;
+        this.cobj.lineWidth=this.lineWidth;
+    },
+    line:function(x,y,x1,y1){
+        var that=this;
+        that.init();
+        that.cobj.beginPath();
+        that.cobj.moveTo(x,y);
+        that.cobj.lineTo(x1,y1);
+        that.cobj.stroke();
+        that.cobj.closePath();
+    },
+    rect:function(x,y,x1,y1){
+        var that=this;
+        that.init();
+        that.cobj.beginPath();
+        that.cobj.rect(x,y,x1-x,y1-y);
+        that.cobj.closePath();
+        that.cobj[that.type]();
+    },
+    arc:function(x,y,x1,y1){
+        var that=this;
+        var r=Math.sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
+        that.init();
+        that.cobj.beginPath();
+        that.cobj.arc(x,y,r,0,Math.PI*2);
+        that.cobj.closePath();
+        that.cobj[that.type]();
+    },
+
+
+    draw:function(){
+        var that=this;
+        that.canvas.onmousedown=function(e){
+            var startx= e.offsetX;
+            var starty= e.offsetY;
+            that.canvas.onmousemove=function(e){
+                that.cobj.clearRect(0,0,that.width,that.height);
+                if(that.history.length!=0){
+                    that.cobj.putImageData(that.history[that.history.length-1],0,0);
+                }
+                var endx= e.offsetX;
+                var endy= e.offsetY;
+                that[that.shapes](startx,starty,endx,endy);
+            }
+            that.canvas.onmouseup=function(){
+                that.history.push(that.cobj.getImageData(0,0,that.width,that.height));
+                that.canvas.onmousemove=null;
+                that.canvas.onmouseup=null;
+            }
+        }
+    },
+    five:function(x,y,x1,y1){
+        var r=Math.sqrt((x1-x)*(x1-x1)+(y1-y)*(y1-y));
+        var r1=r/2;
+        this.cobj.beginPath();
+        this.cobj.moveTo(x+r,y);
+        for(var i=1;i<10;i++){
+            if(i%2==0){
+                this.cobj.lineTo(x+Math.cos(i*36*Math.PI/180)*r,y+Math.sin(i*36*Math.PI/180)*r)
+
+            }else{
+                this.cobj.lineTo(x+Math.cos(i*36*Math.PI/180)*r1,y+Math.sin(i*36*Math.PI/180)*r1)
+            }
+        }
+        this.cobj.closePath();
+        this.cobj[this.type]();
+    },
+    pen:function(){
+        var that=this;
+        that.canvas.onmousedown=function(e){
+            var startx= e.offsetX;
+            var starty= e.offsetY;
+            that.init();
+            that.cobj.beginPath();
+            that.cobj.moveTo(startx,starty);
+            that.canvas.onmousemove=function(e){
+                var endx= e.offsetX;
+                var endy= e.offsetY;
+                that.cobj.lineTo(endx,endy);
+                that.cobj.stroke();
+            }
+            that.canvas.onmouseup=function(){
+                that.cobj.closePath();
+                that.history.push(that.cobj.getImageData(0,0,that.width,that.height));
+                that.canvas.onmousemove=null;
+                that.canvas.onmouseup=null;
+            }
+        }
+    },
+
+
+    xp:function(xpobj,w,h){
+        var that=this;
+        that.canvas.onmousedown=function(){
+            xpobj.css({
+                        display:"block",
+                        width:w+"px",
+                        height:h+"px",
+                 background:"red"
+                    });
+            that.canvas.onmousemove=function(e){
+                var ox= e.offsetX;
+                var oy= e.offsetY;
+                var lefts=ox-w/2;
+                var tops=oy-h/2;
+                if(lefts<0){
+                    lefts=0;
+                }
+                if(lefts>that.width-w){
+                    lefts>that.width-w;
+                }
+                if(tops<0){
+                    tops=0;
+                }
+                if(tops>that.height-h){
+                    tops>that.height-h;
+                }
+                xpobj.css({
+                    left:lefts,
+                    top:tops
+                })
+                that.cobj.clearRect(lefts,tops,w,h);
+
+            }
+            console.log(2);
+            that.canvas.onmouseup=function(e){
+                xpobj.css("display","none");
+                console.log(1);
+                that.history.push(that.cobj.getImageData(0, 0, that.width, that.height));
+                that.canvas.onmousemove = null;
+                that.canvas.onmouseup = null;
+            }
+
+        }
+
+    },
+    ss:function(textArea){
+        var that=this;
+        that.canvas.onmousedown=function(e){
+            var startx= e.offsetX;
+            var starty= e.offsetY,minx,miny, w,h;
+            that.canvas.onmousemove=function(e){
+                var endx= e.offsetX;
+                var endy= e.offsetY;
+                 minx=(startx<endx)?startx:endx;
+                 miny=(starty<endy)?starty:endy;
+                 w=Math.abs(startx-endx);
+                 h=Math.abs(starty-endy);
+                textArea.css({width:w,height:h,left:minx,top:miny});
+            }
+            that.canvas.onmouseup=function(e){
+                that.canvas.onmousemove = null;
+                that.canvas.onmouseup = null;
+                that.temp=that.cobj.getImageData(minx,miny,w,h);
+                that.cobj.clearRect(minx,miny,w,h);
+                that.history.push(that.cobj.getImageData(0,0,that.width,that.height));
+                that.cobj.putImageData(that.temp,minx,miny);
+                that.drag(minx,miny,w,h,textArea);
+            }
+        }
+
+    },
+    drag:function(minx,miny,w,h,textArea){
+        var that=this;
+        that.canvas.onmousemove=function(e){
+            var startx= e.offsetX;
+            var starty= e.offsetY;
+            if(startx>minx&&startx<minx+w&&starty>miny&&starty<miny+h){
+                console.log(1);
+                that.canvas.style.cursor="move";
+            }else{
+                that.canvas.style.cursor="default";
+            }
+        }
+
+        that.canvas.onmousedown=function(e){
+            var startx= e.offsetX;
+            var starty= e.offsetY;
+            var cx=startx-w;
+            var cy=starty-h;
+            if(startx>minx&&startx<minx+w&&starty>miny&&starty<miny+h){
+                that.canvas.style.cursor="move";
+            }else{
+               return;
+            }
+            that.canvas.onmousemove=function(e){
+                that.cobj.clearRect(0,0,that.width,that.height);
+                if(that.history.length!=0){
+                    that.cobj.putImageData(that.history[that.history.length-1],0,0);
+                }
+                var endx= e.offsetX;
+                var endy= e.offsetY;
+                var lefts=endx-cx;
+                var tops=endy-cy;
+                if(lefts<0){
+                    lefts=0;
+                }
+                if(lefts>that.width-w){
+                    lefts=that.width-w
+                }
+
+                if(tops<0){
+                    tops=0;
+                }
+                if(tops>that.height-h){
+                    tops=that.height-h
+                }
+                textArea.css({
+                    left:lefts,
+                    top:tops
+                });
+                x=lefts;
+                y=tops;
+                that.cobj.putImageData(that.temp,lefts,tops);
+            }
+            that.canvas.onmouseup=function(e){
+                that.canvas.onmousemove = null;
+                that.canvas.onmouseup = null;
+            }
+        }
+    }
+}
